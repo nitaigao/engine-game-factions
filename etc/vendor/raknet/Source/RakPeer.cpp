f73ee1c6bf2ca3a037b1189270b4592f36ddc1ae
nkostelnik <nkostelnik@gmail.com>
2009-08-12 14:27:35 +0100

    random typo in raknet

--- etc/vendor/raknet/Source/RakPeer.cpp
+++ etc/vendor/raknet/Source/RakPeer.cpp
@@ -1,4 +1,4 @@
-Send( const RakNet::BitStream * bitStream, PacketPriority priority, PacketReliability reliability, char orderingChannel, SystemAddress systemAddress, bool broadcast )/// \file
+/// \file
 ///
 /// This file is part of RakNet Copyright 2003 Jenkins Software LLC
 ///
